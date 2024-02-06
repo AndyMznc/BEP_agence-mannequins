f6af8d8d560521aed8de78dc01d818fdfe525d4c
AndyMznc <115222358+AndyMznc@users.noreply.github.com>
2024-02-07 00:54:12 +0100

    Amélioration architecture dossiers

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
-import { Elysia } from "elysia";
+import { PrismaClient } from '@prisma/client'
+import { Elysia } from 'elysia'
 
 const app = new Elysia()
-  .group("/models", (app) => {
+  .group('/models', app => {
     return app
-      .get("/", () => "List of models")
-      .post("/", () => "Create a model")
-      .get("/:id", (req) => `Model ${req.params.id}`)
-      .put("/:id", (req) => `Update model ${req.params.id}`)
-      .delete("/:id", (req) => `Delete model ${req.params.id}`);
+      .get('/', () => 'List of models')
+      .post('/', () => 'Create a model')
+      .get('/:id', req => `Model ${req.params.id}`)
+      .put('/:id', req => `Update model ${req.params.id}`)
+      .delete('/:id', req => `Delete model ${req.params.id}`)
   })
-  .listen(3000);
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
+
+const prisma = new PrismaClient()
+
+async function main() {
+  try {
+    const allUsers = await prisma.user.findMany()
+    console.log(allUsers)
+
+    console.log('main call')
+  } catch (error) {
+    console.log(error)
+  }
+}
+main()
